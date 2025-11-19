ece064557a8f13f9128694f0fd4414cdb1eb4507
Clarence254 <nambiroc@gmail.com>
2025-11-19 15:15:49 +0300

    Add SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lusoigardens.com',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     optimizeDeps: {
       exclude: ['jquery', 'popper.js']
